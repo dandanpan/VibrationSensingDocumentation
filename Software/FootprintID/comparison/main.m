e481d0e6062f91080116deb57e35fe6b5cf41f43
worktongyu <47677980+worktongyu@users.noreply.github.com>
2020-09-08 01:07:34 -0700

    Update main.m

--- Software/FootprintID/comparison/main.m
+++ Software/FootprintID/comparison/main.m
@@ -1,12 +1,12 @@
 % number of experiments
-exp_num = 4;
+exp_num = 10;
 
 % run experiments for 4 methods
 % 1) SVM
 % 2) TSVM
 % 3) RTSVM
 % 4) FootprintID
-for exp_id = 4
+for exp_id = 1:4
     cv_sys(exp_id, exp_num, []);
 end
 
